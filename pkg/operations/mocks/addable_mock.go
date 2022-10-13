cbf813bcb8397b200a83f4cceee67fe3887a22e8
Alejandro Visiedo <avisiedo@redhat.com>
2022-10-13 09:08:39 +0200

    test:update mock to use 'Called' instead of 'MethodCalled'
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- pkg/operations/mocks/addable_mock.go
+++ pkg/operations/mocks/addable_mock.go
@@ -12,8 +12,8 @@ func (m *MockAddable) Add(operand1 int, operand2 int) int {
 	// This method only implement the interface
 	// We use the mock methods to:
 	// - register the call to the method.
-	// - retrieve the returns for this call.
-	args := m.MethodCalled("Add", operand1, operand2)
+	// - retrieve the expected returns for this call.
+	args := m.Called(operand1, operand2)
 
 	// The index is the positional returning value
 	return args.Int(0)
